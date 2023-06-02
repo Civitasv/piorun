80bebc91246b8f587c47e83a29725fb914bed536
Civitasv <hscivitasv@gmail.com>
2023-06-02 22:54:47 +0800

    feat: 支持类似于 printf 的格式化输出，支持输出到控制台 or 输出到文件

--- docs/logger.md
+++ docs/logger.md
@@ -1,3 +1,83 @@
 # 日志系统
 
-1. <https://quuxplusone.github.io/blog/2021/11/09/pass-string-view-by-value/>
+目前日志系统支持:
+
+1. 输出到控制台或输出到文件
+2. 格式化输出，类似于 `printf`
+
+## API
+
+```cpp
+  /**
+   * @brief 创建日志实例，输出到文件
+   *
+   * @param logpath 日志输出文件
+   * @return Ref<Logger> 该日志实例可以在多个线程中共享，
+   *         引用数量为0时会被自动回收
+   */
+  static Ref<Logger> Create(const std::string& logpath);
+
+  /**
+   * @brief 创建日志实例，输出到标准输出
+   *
+   * @return Ref<Logger> 该日志实例可以在多个线程中共享，
+   *         引用数量为0时会被自动回收
+   */
+  static Ref<Logger> Create();
+
+  template <typename... T>
+  void Info(const char* __restrict__ fmt, const T&... msg)
+
+  template <typename... T>
+  void Warning(const char* __restrict__ fmt, const T&... msg);
+
+  template <typename... T>
+  void Error(const char* __restrict__ fmt, const T&... msg);
+
+  template <typename... T>
+  void Fatal(const char* __restrict__ fmt, const T&... msg);
+```
+
+## 输出到控制台
+
+```cpp
+int main() {
+  using namespace pio::logger;
+  auto logger = Logger::Create();
+
+  logger->Info("This is a test for %s", "info");
+  logger->Warning("This is a test for %s", "warning");
+  logger->Error("This is a test %s", "error");
+  logger->Fatal("This is a test %s", "fatal");
+}
+```
+
+```txt
+[Info] This is a test for info
+[Warning] This is a test for warning
+[Error] This is a test error
+[Fatal] This is a test fatal
+```
+
+## 输出到文件
+
+```cpp
+int main() {
+  using namespace pio::logger;
+  auto logger = Logger::Create("piorun_test.log"); // 事实上，只有这里被修改了
+
+  logger->Info("This is a test for %s", "info");
+  logger->Warning("This is a test for %s", "warning");
+  logger->Error("This is a test %s", "error");
+  logger->Fatal("This is a test %s", "fatal");
+}
+```
+
+piorun_test.log:
+
+```txt
+[Info] This is a test for info
+[Warning] This is a test for warning
+[Error] This is a test error
+[Fatal] This is a test fatal
+```
--- include/core/log.h
+++ include/core/log.h
@@ -1,44 +1,97 @@
 #ifndef PIORUN_CORE_LOG_H_
 #define PIORUN_CORE_LOG_H_
 
+#include <cstdio>
 #include <fstream>
 #include <mutex>
 #include <ostream>
+#include <sstream>
 
 #include "smartptr.h"
 
 namespace pio {
 namespace logger {
-
-enum Level {
-  INFO = 1 << 0,
-  WARNING = 1 << 1,
-  ERROR = 1 << 2,
-  FATAL = 1 << 3,
-};
-
 class Logger {
  public:
-  Logger(const std::string& logpath) { logfile_.open(logpath); }
-  ~Logger() {
-    if (logfile_.is_open()) logfile_.close();
+  enum Level {
+    INFO = 1 << 0,
+    WARNING = 1 << 1,
+    ERROR = 1 << 2,
+    FATAL = 1 << 3,
+  };
+
+ public:
+  Logger() : use_file_(false) {}
+  Logger(const std::string& logpath) : use_file_(true) {
+    logfile_ = fopen(logpath.c_str(), "w");
   }
 
+  ~Logger() {
+    if (use_file_) fclose(logfile_);
+  }
+
+  /**
+   * @brief 创建日志实例，输出到文件
+   *
+   * @param logpath 日志输出文件
+   * @return Ref<Logger> 该日志实例可以在多个线程中共享，
+   *         引用数量为0时会被自动回收
+   */
   static Ref<Logger> Create(const std::string& logpath);
 
-  inline void Info(const std::string& msg) { Log(INFO, msg); }
-  inline void Warning(const std::string& msg) { Log(WARNING, msg); }
-  inline void Error(const std::string& msg) { Log(ERROR, msg); }
-  inline void Fatal(const std::string& msg) { Log(FATAL, msg); }
+  /**
+   * @brief 创建日志实例，输出到标准输出
+   *
+   * @return Ref<Logger> 该日志实例可以在多个线程中共享，
+   *         引用数量为0时会被自动回收
+   */
+  static Ref<Logger> Create();
+
+  template <typename... T>
+  inline void Info(const char* __restrict__ fmt, const T&... msg) {
+    Log(INFO, fmt, msg...);
+  }
+
+  template <typename... T>
+  inline void Warning(const char* __restrict__ fmt, const T&... msg) {
+    Log(WARNING, fmt, msg...);
+  }
+
+  template <typename... T>
+  inline void Error(const char* __restrict__ fmt, const T&... msg) {
+    Log(ERROR, fmt, msg...);
+  }
+
+  template <typename... T>
+  inline void Fatal(const char* __restrict__ fmt, const T&... msg) {
+    Log(FATAL, fmt, msg...);
+  }
 
  private:
-  void Log(Level l, const std::string& msg);
+  template <typename... T>
+  void Log(Level l, const char* __restrict__ fmt, const T&... msg) {
+    std::lock_guard<std::mutex> lk(mt_);
+    auto level = LevelToString(l);
+
+    if (use_file_) {
+      fprintf(logfile_, "%s ", level.c_str());
+      fprintf(logfile_, fmt, msg...);
+      fprintf(logfile_, "\n");
+    } else {
+      printf("%s ", level.c_str());
+      printf(fmt, msg...);
+      printf("\n");
+    }
+  }
 
  private:
-  std::ofstream logfile_;
+  std::string LevelToString(Level l);
+
+ private:
+  FILE* logfile_;
+  bool use_file_;
   std::mutex mt_;
 };
-
 }  // namespace logger
 }  // namespace pio
 
