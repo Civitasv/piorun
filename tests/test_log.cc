ac1e7f149676944051915d7c64a8fa2181521852
Civitasv <hscivitasv@gmail.com>
2023-06-03 11:07:21 +0800

    logger: 添加流式输出方法

--- tests/test_log.cc
+++ tests/test_log.cc
@@ -3,14 +3,24 @@
 
 #include "core/log.h"
 
-void basic_test() {
+void basic_test_with_format() {
   using namespace pio::logger;
   auto logger = Logger::Create();
 
-  logger->Info("This is a test for %s", "info");
-  logger->Warning("This is a test for %s", "warning");
-  logger->Error("This is a test %s", "error");
-  logger->Fatal("This is a test %s", "fatal");
+  logger->InfoF("This is a test for %s", "info");
+  logger->WarningF("This is a test for %s", "warning");
+  logger->ErrorF("This is a test for %s", "error");
+  logger->FatalF("This is a test for %s", "fatal");
+}
+
+void basic_test_without_format() {
+  using namespace pio::logger;
+  auto logger = Logger::Create();
+
+  logger->Info("This is a test for ", "info");
+  logger->Warning("This is a test for ", "warning");
+  logger->Error("This is a test for ", "error");
+  logger->Fatal("This is a test for ", "fatal");
 }
 
 void test_in_thread() {
@@ -18,7 +28,8 @@ void test_in_thread() {
   auto logger = Logger::Create();
 
   auto func = [logger]() {
-    logger->Info("%s", "This is a test for thread");
+    logger->Info(std::this_thread::get_id());
+    logger->InfoF("%s", "This is a test for thread");
   };
 
   std::thread t1(func);
@@ -28,4 +39,14 @@ void test_in_thread() {
   t2.join();
 }
 
-int main() { test_in_thread(); }
+void test_stream_out() {
+  using namespace pio::logger;
+  auto logger = Logger::Create();
+
+  logger << "TEST "
+         << "For "
+         << "Stream "
+         << "Out " << 1 << '\n';
+}
+
+int main() { test_stream_out(); }
