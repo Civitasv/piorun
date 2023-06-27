56a23f748bebedd358407c94aa0f4b8e18215f8f
husen <husen1@xiaomi.com>
2023-06-27 15:23:58 +0800

    scheduler: still some comments

--- src/piorun/coroutine/async/async_server.cc
+++ src/piorun/coroutine/async/async_server.cc
@@ -10,8 +10,10 @@ namespace pio {
 task::Chainable AsyncServer(
     SocketView s, std::function<task::Terminating(Socket)> connection_handler) {
   while (true) {
-    if (auto result = co_await AsyncAccept(s, connection_handler); !result)
+    if (auto result = co_await AsyncAccept(s, connection_handler); !result) {
+      // 如果 AsyncAccept 不返回 WAKEUP，则服务端结束监听，否则继续添加 accept 事件
       co_return result;
+    }
   }
 }
 
