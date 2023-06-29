76525a611554e0f28e0f8a838c309d125d792195
jiongpaichengxuyuan <62229819+jiongpaichengxuyuan@users.noreply.github.com>
2023-06-29 18:33:28 +0800

    feat: rio and echo server test (#11)
    * feat: rio and echo server test
    
    * delete annotation

--- include/coroutine/async/async_write.h
+++ include/coroutine/async/async_write.h
@@ -2,13 +2,15 @@
 #define PIORUN_COROUTINE_ASYNC_WRITE_H_
 
 #include <span>
+
 #include "coroutine/task/chainable.h"
 #include "socket.h"
 
 namespace pio {
 
-task::Chainable AsyncWrite(SocketView s, std::span<const std::byte> &data);
+task::Chainable AsyncWrite(SocketView s, std::span<const std::byte>& data);
 
-}
+task::Chainable AsyncWrite(SocketView s, char* buffer, size_t n);
+}  // namespace pio
 
 #endif  // PIORUN_COROUTINE_ASYNC_WRITE_H_
--- src/piorun/coroutine/async/async_write.cc
+++ src/piorun/coroutine/async/async_write.cc
@@ -7,7 +7,7 @@
 
 namespace pio {
 
-task::Chainable AsyncWrite(SocketView s, std::span<const std::byte> &data) {
+task::Chainable AsyncWrite(SocketView s, std::span<const std::byte>& data) {
   while (data.size() > 0) {
     ssize_t cnt = write(s->fd_, data.data(), data.size());
     if (cnt == -1 && errno != EAGAIN)
@@ -22,6 +22,31 @@ task::Chainable AsyncWrite(SocketView s, std::span<const std::byte> &data) {
                                                      s->fd_, s.deadline());
         !status) {
       co_return status;
+    }
+  }
+  co_return awaitable::Result{EventType::WAKEUP, 0, ""};
+}
+
+// write其实不需要等待，因为写缓存区基本都是可写的
+// 并且如果将写操作也注册为事件，会出现bug
+// 主要就在于epoll_wait可能感知不到socket可写，所以并不会执行真正的写操作
+// 因此client就会一直被搁置
+task::Chainable AsyncWrite(SocketView s, char* buffer, size_t n) {
+  size_t nleft = n;
+  ssize_t nwritten;
+  char* buf = buffer;
+  while (nleft > 0) {
+    nwritten = write(s->fd_, buf, nleft);
+    if (nwritten < 0) {
+      if (errno == EAGAIN) {
+        break;
+      } else {
+        co_return awaitable::Result{EventType::ERROR, errno,
+                                    "Failed to write data to socket."};
+      }
+    } else {
+      nleft -= nwritten;
+      buf += nwritten;
     }
   }
   co_return awaitable::Result{EventType::WAKEUP, 0, ""};
