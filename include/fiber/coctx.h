6b54a7bb062d11fc49eb9133b71e6c1702c3497e
jiongpaichengxuyuan <570073523@qq.com>
2023-07-11 05:17:49 -0400

    fiber: fix some errors

--- include/fiber/coctx.h
+++ include/fiber/coctx.h
@@ -4,7 +4,7 @@
  * @brief 有栈协程
  * @version 0.1
  * @date 2023-06-12
- * 
+ *
  * @copyright Copyright (c) 2023
  */
 
@@ -17,17 +17,14 @@ namespace pio::fiber {
 
 // 协程的上下文
 struct FiberContext {
-
   struct StackMemory {
-    
-    int   stackSize_; /*> 栈的大小 */
-    char*        bp_; /*> 栈基址 */
-    char*    buffer_; /*> 栈顶，stack_bp = stack_buffer + stack_size */
+    int stackSize_; /*> 栈的大小 */
+    char* bp_;      /*> 栈基址 */
+    char* buffer_;  /*> 栈顶，stack_bp = stack_buffer + stack_size */
 
     StackMemory(int size = 128 * 1024);
 
-   ~StackMemory();
-
+    ~StackMemory();
   };
 
   FiberContext();
@@ -36,7 +33,7 @@ struct FiberContext {
 
   FiberContext(FiberContext&& rhs) noexcept;
 
- ~FiberContext();
+  ~FiberContext();
 
   FiberContext& operator=(const FiberContext& rhs);
 
@@ -51,13 +48,14 @@ struct FiberContext {
    * @param s2  @p pfn 的第二个参数
    * @return int
    */
-  int Make(void*(*pfn)(void*, void*), const void *s1, const void* s2);
+  int Make(void* (*pfn)(void*, void*), const void* s1, const void* s2);
 
-  void*     regs_[14]; /*> 存储13个通用寄存器和rip，这里不需要r10，r11以及rax，r10、r11和rax会被调用者保存 */
+  void* regs_[13];     /*>
+                          存储13个通用寄存器，这里不需要r10，r11以及rax，r10、r11和rax会被调用者保存
+                        */
   StackMemory* stack_; /*> 栈内存 */
-
 };
 
-}
+}  // namespace pio::fiber
 
 #endif
